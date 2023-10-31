6381a7f7d89c5296264088d2a42ac2f65b3337ce
Jiang <zhou.jiang@fiserv.com>
2023-10-31 14:24:52 -0400

    add solution

--- Solution.java
+++ Solution.java
@@ -1,7 +1,25 @@
+import java.util.Collections;
+import java.util.HashMap;
+
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+        HashMap<Character, Integer> map = new HashMap<>();
+        char ans = 'a';
+        for(int i =0;i<s.length();i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+            int max = -1;
+            for (char a : map.keySet()) {
+                if (map.get(a) > max) {
+                    max = map.get(a);
+                    ans = a;
+                }
+            }
+        }
+        return ans;
     }
   
     public static void main(String[] args) {
---
+++ out/production/AH5/README.md
@@ -0,0 +1,25 @@
+# Algo Hour 5
+
+## Objectives
+
+Write a function, mostFrequentChar, that takes in a string as an argument. 
+The function should return the most frequent character of the string. If there are ties, 
+the character that appears earlier in the string. You can assume that the input string is non-empty.
+
+## Test Cases
+
+```java
+System.out.println(Source.mostFrequentChar("bookeeper")); // -> 'e'
+```
+
+```java
+System.out.println(Source.mostFrequentChar("david")); // -> 'd'
+```
+
+```java
+System.out.println(Source.mostFrequentChar("mississippi")); // -> 'i'
+```
+
+```java
+System.out.println(Source.mostFrequentChar("eleventennine")); // -> 'e'
+```
