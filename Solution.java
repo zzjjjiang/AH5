bb6669d4bf4b463bb2b25c513eae25dad8525822
Dhruv T <dhruv.thota@fiserv.com>
2023-10-31 10:06:21 -0400

    initializing repo...

---
+++ Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static char mostFrequentChar(String s) {
+      
+      return;
+    }
+  
+    public static void main(String[] args) {
+      // Try out your test cases:
+      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+    }
+
+  }
